SonalSingh18/android_packages_apps_ZenParts

[SQUASH] sdm660-common: Import ZenParts v2.0
Features:-
• Enable/Disable Camera2 API Toggle
• Torch Brightness Control
• Vibration Control
• Display Color Calibration
• Grayscale & KCAL Presets
• Spectrum Profiles
• Dirac Sound Enhancer
• Headphone Gain
• Microphone Gain
• Added Spectrum Support

Refrenced from:- https://github.com/xiaomi-sdm660/android_device_xiaomi_sdm660-common.git

Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

rootdir/Android.mk
@@ -97,3 +97,37 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+# Spectrum scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.spectrum.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.spectrum.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.spectrum.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.spectrum.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+# ZenParts scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.zenparts.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.zenparts.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.zenparts.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := bin/init.zenparts.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)

strongreasons/android_device_asus_sdm660-common

Signed-off-by: Kneba <abenkenary3@gmail.com>

rootdir/Android.mk
@@ -97,3 +97,37 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/device_check.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+# Spectrum scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.spectrum.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.spectrum.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.spectrum.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := bin/init.spectrum.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+# ZenParts scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.zenparts.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.zenparts.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.zenparts.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := bin/init.zenparts.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)